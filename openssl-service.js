77b4b9cf9430831824765e88f2af094c9e720c9e
Rolando Santamaria Maso <kyberneees@gmail.com>
2023-01-12 06:16:04 +0100

    refactoring openssl service

--- openssl-service.js
+++ openssl-service.js
@@ -1,5 +1,20 @@
 const openssl = require('async-openssl')
+const fs = require('fs/promises')
 
-module.exports = function (command) {
-  return openssl(command)
+module.exports = {
+  exec (command) {
+    return openssl(command)
+  },
+  async  getAndDeleteKeyPair (filePrivKey, filePubKey) {
+    const [publicKey, privateKey] = await Promise.all([
+      fs.readFile(filePubKey, 'utf8'),
+      fs.readFile(filePrivKey, 'utf8')
+    ])
+    await Promise.all([
+      fs.unlink(filePubKey),
+      fs.unlink(filePrivKey)
+    ])
+
+    return { publicKey, privateKey }
+  }
 }
